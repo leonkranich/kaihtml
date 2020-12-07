01737e5f4b27d50849c6a999a90f3c6e849a5c7d
leon kranich <leon_kranich@yahoo.de>
2020-12-07 14:01:09 +0100

    mailto for mobile

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.getElementById("email").value = "leon_kranich@yahoo.de"
-document.getElementById("newAction").action = "mailto:leon_kranich@yahoo.de"
+document.getElementById("newAction").action = "mailto:leon_kranich@yahoo.de?"
 
 const myAlert = () => {
   alert("Thanks, your question has been sent!");
